0cd41b777c94026641efd4fe7322ee38f1db4851
oliv <oliv@Wall-E.(none)>
2012-02-24 23:53:49 +0100

    Ajout d'un paramètre pour préciser l'espèce

--- src/mIAmIAm.cpp
+++ src/mIAmIAm.cpp
@@ -1,17 +1,30 @@
 // mIAmIAm.cpp�d�finit le point d'entr�e pour l'application
 
 #include "IA/joueurIA.h"
+#include <cstring>
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-	if (argc<3) {
-		cout << "Configurer l'execution avec <host> <port>" << endl;
+	if (argc<4) {
+		cerr << "Configurer l'execution avec <host> <port> <espece>" << endl;
 		return -1;
 	}
 	try {
-		JoueurIA* nous = new JoueurIA("mIAm", argv[1], argv[2]);
+		Espece especeJoueur;
+		if (0==strcmp(argv[3], "vampire")) {
+			especeJoueur = VAMPIRE;
+		}
+		else if (0==strcmp(argv[3], "loup")) {
+			especeJoueur = LOUP;
+		}
+		else {
+			cerr << "Espece incorrecte. Choix possibles: 'vampire', 'loup'." << endl;
+			return -1;
+		}
+
+		JoueurIA* nous = new JoueurIA("mIAm", argv[1], argv[2], especeJoueur);
 		nous->preparerPartie();
 		nous->jouer();
 
